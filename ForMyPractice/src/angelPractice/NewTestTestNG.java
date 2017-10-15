b5b8b73960bb77a310801146e4e45a5d71220122
Angel <angu.saran.92@gmail.com>
2017-10-15 19:46:52 +0530

    jswfghsfhs

---
+++ ForMyPractice/src/angelPractice/NewTestTestNG.java
@@ -0,0 +1,19 @@
+package angelPractice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class NewTestTestNG {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
